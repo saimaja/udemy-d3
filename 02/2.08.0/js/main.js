0f596c96471a34b9b1320ef75f89acce69a6cb1e
saimaja <saimaakhtar@gmail.com>
2020-11-21 20:05:20 -0500

    2.08.0

--- 02/2.08.0/js/main.js
+++ 02/2.08.0/js/main.js
@@ -3,3 +3,23 @@
 *    Mastering Data Visualization with D3.js
 *    2.8 - Activity: Your first visualization!
 */
+
+let svg = d3.select('#chart-area').append('svg').attr('width', 500).attr('height', 500)
+
+
+d3.json('/data/buildings.json').then(data => {
+    data.forEach(d => {
+        d.height = Number(d.height)
+    })
+
+    let rectangles = svg.selectAll('rect').data(data)
+
+    rectangles.enter().append('rect')
+        .attr('x', (d, i) => (i * 60))
+        .attr('y', 50)
+        .attr('width', 30)
+        .attr('height', d => d.height)
+        .attr('fill', 'blue')
+
+})
+
