d4d3c86f6a559cfeac79f290ddc696fdbab621ec
saimaja <saimaakhtar@gmail.com>
2020-11-27 18:40:56 -0500

    bar chart

--- 03/3.02/js/main.js
+++ 03/3.02/js/main.js
@@ -22,8 +22,8 @@ d3.json("data/buildings.json").then(data => {
   
   rects.enter().append("rect")
     .attr("y", 0)
-    .attr("x", (d, i) => (i * 60))
-    .attr("width", 40)
+    .attr("x", (d, i) => (i * 50))
+    .attr("width", 30)
     .attr("height", d => y(d.height))
-    .attr("fill", "grey")
+    .attr("fill", "red")
 })
